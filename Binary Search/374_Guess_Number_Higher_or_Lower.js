9b4960cb718829855aa2239b668b7b24dcd8ea51
Harish-coding <77202887+Harish-coding@users.noreply.github.com>
2022-05-08 14:30:43 +0300

    Add files for Binary Search I, day 1.

---
+++ Binary Search/374_Guess_Number_Higher_or_Lower.js
@@ -0,0 +1,36 @@
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var guessNumber = function(n) {
+    let start = 1;
+    let end = n;
+    
+    while (true) {
+        let mid = Math.floor((start + end) / 2);
+    
+        let result = guess(mid);
+        // found target, return index
+        if (result == 0) {
+            return mid;
+        }
+    
+        if (result ==  -1) {
+            end = mid -1;
+        }  
+    
+        if (result == 1) {
+            start = mid + 1;
+        }
+    }
+    
+};
---
+++ Binary Search/704_Binary_Search.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+
+ var search = function(nums, target) {
+    let start_idx = 0;
+    let end_idx = nums.length - 1;
+    
+    while (start_idx <= end_idx) {
+        let mid_idx = Math.floor((start_idx + end_idx) / 2);
+    
+        // found target, return index
+        if (nums[mid_idx] == target) {
+            return mid_idx;
+        }
+    
+        if (nums[mid_idx] < target) {
+            start_idx = mid_idx + 1;
+        }   
+    
+        if (nums[mid_idx] > target) {
+            end_idx = mid_idx - 1;
+        }
+    }
+    
+    return -1;
+}
+   
